e802dcace629d701b1836fc3a439439a4b28230a
Rokon007 <rokon@example.com>
2026-01-07 22:15:23 +0600

    added a commit

--- A1.js
+++ A1.js
@@ -17,3 +17,12 @@ for (i=1; i<=10; i++)
 
 }
 //
+let i=1;
+let sum=0;
+for (i=1; i<=10; i++)
+{
+    sum+=i;
+    console.log("value of i is:",i);
+    console.log (" Sum is :", sum);
+
+}
---
+++ A2.js
@@ -0,0 +1,13 @@
+const fruits =['apple', 'bannana', 'cherry'];
+console.log(fruits);
+//
+console.log("first fruit is:", fruits[0]);
+console.log ("3rd fruit is:", fruits[2]);
+fruits.push('orange');
+console.log(fruits);
+// 
+const person= ['1st person', '2nd person', '3rd person'];
+console.log("1st person is:", person[0]);
+person.unshift('no person');
+person.push('last person');
+console.log(person);
