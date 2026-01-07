07ccc719a05aded5d87695b570cddc6a301fc0e9
Rokon007 <rokon@example.com>
2026-01-07 21:59:24 +0600

    added a commit

---
+++ A1.js
@@ -0,0 +1,19 @@
+var A=1;
+var B=2;
+console.log("sum:", A+B);
+// 
+var name="ROKON";
+console.log("Name:", name);
+// 
+var a=10;
+var b=20;
+console.log ("value of A & B is :", a,b);
+
+// loop 
+var i=1;
+for (i=1; i<=10; i++)
+{
+    console.log("Value of i is :", i);
+
+}
+//
